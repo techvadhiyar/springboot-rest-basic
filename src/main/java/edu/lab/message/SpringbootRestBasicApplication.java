fdfe49566690af2bd3ef81aa18b79635225ac187
cpccharry <harry.subramaniam@cpcc.edu>
2020-10-24 14:05:14 -0400

    Initial commit

---
+++ src/main/java/edu/lab/message/SpringbootRestBasicApplication.java
@@ -0,0 +1,13 @@
+package edu.lab.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lab/message/SpringbootRestBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lab.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
